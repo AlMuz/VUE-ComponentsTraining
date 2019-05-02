1b7e0ed54f679525796b8f8b72dbd2e56dfa8629
Aleksejs Muzičenko <aleksejs@mendo.lv>
2019-05-02 16:21:00 +0300

    rendering articles in card

--- src/js/app.js
+++ src/js/app.js
@@ -17,7 +17,20 @@ new Vue({
   el: '#app',
 
   data: {
-
+    articles: [{
+      title: 'Simple title 1',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ',
+    }, {
+      title: 'Simple title 2',
+      content: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+    }, {
+      title: 'Simple title 3',
+      content: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    }, {
+      title: 'Simple title 4',
+      content: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    }
+    ]
   },
 
 });
